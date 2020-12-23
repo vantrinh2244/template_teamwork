86b4595e6fb79893973247e163a5b77d19655c31
Phi Bao <baopham14714714@gmail.com>
2020-12-23 17:17:18 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+   
+
+    // function animatethis(targetElement, speed) {
+    //     var scrollWidth = $(targetElement).get(0).scrollWidth;
+    //     var clientWidth = $(targetElement).get(0).clientWidth;
+    //     $(targetElement).animate({ scrollLeft: scrollWidth - clientWidth },
+    //     {
+    //         duration: speed,
+    //         complete: function () {
+    //             targetElement.animate({ scrollLeft: 0},
+    //             {
+    //                 duration: speed,
+    //                 complete: function () {
+    //                     animatethis(targetElement, speed);
+    //                 }
+    //             });
+    //         }
+    //     });
+    // };
+    // animatethis($('#banner_header_pc'), 5000);
+});
